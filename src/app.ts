2ae062d11cdd12e4692f937b8091c6497906e692
Ash Mackay <ash.mackay85@gmail.com>
2022-04-18 16:06:50 +0100

    Generic Utility Types

--- src/app.ts
+++ src/app.ts
@@ -78,3 +78,25 @@ const numberStorage = new DataStorage<number>();
 // // ...
 // objStorage.removeItem(ashObj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Ash", "Jasmine"];
+// names.push('Yoshi');
+// names.pop();
